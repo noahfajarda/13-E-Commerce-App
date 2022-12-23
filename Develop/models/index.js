d636db67fec5b21c48b31b8613f2f4d0adc004e7
Noah Fajarda <noahfajarda@gmail.com>
2022-12-23 14:12:54 -0800

    draft done for product routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,29 +5,55 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-    foreignKey: "category_id",
-    onDelete: "CASCADE",
-});
+// Product.belongsTo(Category, {
+//     foreignKey: "category_id",
+//     onDelete: "CASCADE",
+// });
 
+// // Categories have many Products
+// Category.hasMany(Product, {
+//     foreignKey: "category_id",
+// });
+
+// // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Tag, {
+//     through: { model: ProductTag, unique: false },
+//     as: "products_tag_testing",
+//     // foreignKey: "product_ID",
+// });
+
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(Product, {
+//     through: { model: ProductTag, unique: false },
+//     // as: "tag_product",
+//     // foreignKey: "tag_ID",
+// });
+
+// WORKS!!!!
+// Products belongsTo Category
+Product.belongsTo(Category);
 // Categories have many Products
-Category.hasMany(Product, {
-    foreignKey: "category_id",
-});
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-    through: { model: ProductTag, unique: false },
-    as: "products_tag_testing",
-    // foreignKey: "product_ID",
+    through: ProductTag,
+    foreignKey: "product_id",
 });
-
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-    through: { model: ProductTag, unique: false },
-    // as: "tag_product",
-    // foreignKey: "tag_ID",
+    through: ProductTag,
+    foreignKey: "tag_id",
 });
+
+// ONE - TO - ONE === hasOne() & belongsTo()
+// ONE - TO - MANY === hasMany() & belongsTo()
+// MANY - TO - MANY === belongsToMany()
+
+// TESTING
+// Product.hasOne(Category, {
+//     foreignKey: "category_id",
+//     onDelete: "CASCADE",
+// });
 
 module.exports = {
     Product,
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,11 +4,20 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-// DONE DONE DONE DONE DONE DONE
+// DONE DONE DONE DONE DONE
 router.get("/", async (req, res) => {
     try {
         const productData = await Product.findAll({
-            include: [{ model: Category }],
+            include: [
+                {
+                    model: Category,
+                },
+                {
+                    model: Tag,
+                    through: ProductTag,
+                },
+            ],
+            attributes: ["id", "product_name", "price", "stock", "category_id"],
         });
         res.status(200).json(productData);
     } catch (err) {
@@ -19,12 +28,26 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     // find a single product by its `id`
     // be sure to include its associated Category and Tag data
-    Product.findByPk(req.params.id).then((productData) => {
-        res.json(productData);
-    });
+    try {
+        const productData = await Product.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Category,
+                },
+                {
+                    model: Tag,
+                    through: ProductTag,
+                },
+            ],
+            attributes: ["id", "product_name", "price", "stock", "category_id"],
+        });
+        res.status(200).json(productData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // create new product
